92ed4f4977ff344c39a93953171762279e9bdb32
antmoute <lejugeti@gmail.com>
2025-02-15 14:43:03 +0100

    db add transaction mode

--- public/db/db.js
+++ public/db/db.js
@@ -1,29 +1,38 @@
 const pgp = require('pg-promise')();
-const fs = require('fs');
+const { TransactionMode, isolationLevel } = pgp.txMode;
+
+const fs = require("fs");
 
 const pgConfig = {
-  host: 'localhost',
-  database: 'postgres',
-  user: 'user',
-  password: 'password',
-  port: '5432',
+  host: "localhost",
+  database: "postgres",
+  user: "user",
+  password: "password",
+  port: "5432",
   ssl: {
     ca: fs.readFileSync(
-      '/Users/antmoute/Documents/dev/parking/parking-backend/config/security/ca.crt'
+      "/Users/antmoute/Documents/dev/parking/parking-backend/config/security/ca.crt"
     ),
     cert: fs.readFileSync(
-      '/Users/antmoute/Documents/dev/parking/parking-backend/config/security/parking_app_cert.pem'
+      "/Users/antmoute/Documents/dev/parking/parking-backend/config/security/parking_app_cert.pem"
     ),
     key: fs.readFileSync(
-      '/Users/antmoute/Documents/dev/parking/parking-backend/config/security/parking_app_key.pem'
+      "/Users/antmoute/Documents/dev/parking/parking-backend/config/security/parking_app_key.pem"
     ),
-    passphrase: 'password',
+    passphrase: "password",
     rejectUnauthorized: true,
   },
 };
 
 const db = pgp(pgConfig);
 
+const transactionMode = new TransactionMode({
+  tiLevel: isolationLevel.serializable,
+  readOnly: true,
+  deferrable: true,
+});
+
 module.exports = {
-  db
+  db,
+  transactionMode,
 };
