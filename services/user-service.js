9072d09dec1b5ad514fcb81227bc5576c767f39f
antmoute <lejugeti@gmail.com>
2025-02-16 20:32:15 +0100

    userService as class

--- services/user-service.js
+++ services/user-service.js
@@ -1,20 +1,20 @@
 const { PreparedStatement: PS } = require("pg-promise");
 const { db } = require("../public/db/db");
 
-function getUser(userId) {
-    if(!userId) {
-        throw new Error("User id can not be null");
+class UserService {
+  getUserById(userId) {
+    if (!userId) {
+      throw new Error("User id can not be null");
     }
 
     const findUser = new PS({
-        name: "get-user",
-        text: "select * from users where id = $1",
-        values: [userId]
-    })
+      name: "get-user-by-id",
+      text: "select * from users where id = $1",
+      values: [userId],
+    });
 
     return db.oneOrNone(findUser);
+  }
 }
 
-module.exports = {
-    getUser
-}
+module.exports = new UserService();
