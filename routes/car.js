0efc9665ba72f4d4f23e0225dbb225fa6bd11642
antmoute <lejugeti@gmail.com>
2025-02-20 19:45:06 +0100

    add endpoint to enable user to modify a car's parking location

--- routes/car.js
+++ routes/car.js
@@ -7,6 +7,7 @@ const parkLocationService = require("../services/park-location-service");
 const authenticationService = require("../services/authentication-service");
 const userService = require("../services/user-service");
 const IllegalArgumentError = require("../public/errors/illegal-argument.error");
+const NotFoundError = require("../public/errors/not-found.error");
 
 router.get("/:carId", async (req, res, next) => {
   const carId = req.params.carId;
@@ -92,5 +93,37 @@ router.post("/:carId/park-location", async (req, res, next) => {
   }
 });
 
+router.put("/:carId/park-location/:parkLocationId", async (req, res, next) => {
+  try {
+    const { carId, parkLocationId } = req.params;
+    const { login } = authenticationService.getAuthFromRequest(req);
+    const updaterUser = await userService.getUserByLogin(login);
+    await parkLocationService.updateParkLocation(
+      carId,
+      parkLocationId,
+      updaterUser,
+      req.body
+    );
+
+    res.send();
+  } catch (err) {
+    if (err instanceof IllegalArgumentError) {
+      next(createHttpError(400, err.message));
+      return;
+    } else if (err instanceof NotFoundError) {
+      next(createHttpError(404, err.message));
+      return;
+    }
+
+    console.error(err);
+
+    next(
+      createHttpError(
+        500,
+        "Internal error occured while creating parking location"
+      )
+    );
+  }
+});
 
 module.exports = router;
