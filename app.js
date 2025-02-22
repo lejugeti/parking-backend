fa04e1a3fbe23ac30810ec46cc966a25a5e0b78e
antmoute <lejugeti@gmail.com>
2025-02-22 12:14:10 +0100

    use helmet package to enhance security

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require("helmet");
+
 var authenticationService = require("./services/authentication-service");
 
 var indexRouter = require("./routes/index");
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(helmet());
 
 app.use(async function (req, res, next) {
   try {
