b75eb583e84be8415e4eeda8055e0af33307bef0
antmoute <lejugeti@gmail.com>
2025-03-02 18:57:52 +0100

    apply validation on all users endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { param, body, validationResult } = require("express-validator");
 const createHttpError = require("http-errors");
 const uuidService = require("../services/uuid-service");
 const carService = require("../services/car-service");
@@ -17,110 +18,142 @@ const authValidator = new AuthorizationValidator();
 /**
  * Update username
  */
-router.put("/:userId/username", async (req, res, next) => {
-  const { userId } = req.params;
-  const { username } = req.body;
+router.put(
+  "/:userId/username",
+  [
+    param("userId")
+      .notEmpty()
+      .trim()
+      .custom((id) => uuidService.isUUID(id))
+      .escape(),
+    body("username").notEmpty().trim().escape(),
+  ],
+  async (req, res, next) => {
+    const reqValidation = validationResult(req);
+    const { userId } = req.params;
+    const { username } = req.body;
 
-  if (!uuidService.isUUID(userId)) {
-    next(createHttpError(400, "User id is invalid"));
-    return;
-  } else if (!username || username.length === 0) {
-    next(createHttpError(400, "Username is invalid"));
-    return;
-  }
-
-  try {
-    const login = authenticationService.getAuthFromRequest(req).login;
-    const user = await userService.getUserByLogin(login);
-    await authValidator.validate(new UserTargetsItself(db, user.id, userId));
-
-    await userService.modifyUsername(userId, username);
-    res.send();
-  } catch (err) {
-    if (err instanceof IllegalArgumentError) {
-      next(createHttpError(400, err.message));
-      return;
-    } else if (err instanceof NotFoundError) {
-      next(createHttpError(404, err.message));
-      return;
-    } else if (err instanceof UnauthorizedError) {
-      next(createHttpError(403, err.message));
+    if (!reqValidation.isEmpty()) {
+      const error = reqValidation.errors[0];
+      next(createHttpError(400, `Bad request : ${error.path} is invalid`));
       return;
     }
 
-    console.error(err);
+    try {
+      const login = authenticationService.getAuthFromRequest(req).login;
+      const user = await userService.getUserByLogin(login);
+      await authValidator.validate(new UserTargetsItself(db, user.id, userId));
 
-    next(createHttpError(500, "Internal error occured updating user"));
+      await userService.modifyUsername(userId, username);
+      res.send();
+    } catch (err) {
+      if (err instanceof IllegalArgumentError) {
+        next(createHttpError(400, err.message));
+        return;
+      } else if (err instanceof NotFoundError) {
+        next(createHttpError(404, err.message));
+        return;
+      } else if (err instanceof UnauthorizedError) {
+        next(createHttpError(403, err.message));
+        return;
+      }
+
+      console.error(err);
+
+      next(createHttpError(500, "Internal error occured updating user"));
+    }
   }
-});
+);
 
 /**
  * Get user's car list
  */
-router.get("/:userId/car-list", async (req, res, next) => {
-  const { userId } = req.params;
+router.get(
+  "/:userId/car-list",
+  [
+    param("userId")
+      .notEmpty()
+      .trim()
+      .custom((id) => uuidService.isUUID(id))
+      .escape(),
+  ],
+  async (req, res, next) => {
+    const reqValidation = validationResult(req);
+    const { userId } = req.params;
 
-  if (!uuidService.isUUID(userId)) {
-    next(createHttpError(400, "User id is invalid"));
-    return;
-  }
-
-  try {
-    const login = authenticationService.getAuthFromRequest(req).login;
-    const user = await userService.getUserByLogin(login);
-    await authValidator.validate(new UserTargetsItself(db, user.id, userId));
-
-    const userCars = await userService.getUserCars(userId);
-    res.send(userCars);
-  } catch (err) {
-    if (err instanceof IllegalArgumentError) {
-      next(createHttpError(400, err.message));
-      return;
-    } else if (err instanceof UnauthorizedError) {
-      next(createHttpError(403, err.message));
+    if (!reqValidation.isEmpty()) {
+      const error = reqValidation.errors[0];
+      next(createHttpError(400, `Bad request : ${error.path} is invalid`));
       return;
     }
 
-    next(createHttpError(500, "Internal error while fetching user cars"));
+    try {
+      const login = authenticationService.getAuthFromRequest(req).login;
+      const user = await userService.getUserByLogin(login);
+      await authValidator.validate(new UserTargetsItself(db, user.id, userId));
+
+      const userCars = await userService.getUserCars(userId);
+      res.send(userCars);
+    } catch (err) {
+      if (err instanceof IllegalArgumentError) {
+        next(createHttpError(400, err.message));
+        return;
+      } else if (err instanceof UnauthorizedError) {
+        next(createHttpError(403, err.message));
+        return;
+      }
+
+      next(createHttpError(500, "Internal error while fetching user cars"));
+    }
   }
-});
+);
 
 /**
  * Create a car for specific user
  */
-router.post("/:userId/car", async (req, res, next) => {
-  const { userId } = req.params;
-  const { carName } = req.body;
+router.post(
+  "/:userId/car",
+  [
+    param("userId")
+      .notEmpty()
+      .trim()
+      .custom((id) => uuidService.isUUID(id))
+      .escape(),
+    body("carName").notEmpty().trim().escape(),
+  ],
+  async (req, res, next) => {
+    const reqValidation = validationResult(req);
+    const { userId } = req.params;
+    const { carName } = req.body;
 
-  if (!uuidService.isUUID(userId)) {
-    next(createHttpError(400, "User id is invalid"));
-    return;
-  } else if (!carName || carName.length === 0) {
-    next(createHttpError(400, "Car name must not be null or empty"));
-    return;
-  }
-
-  try {
-    const login = authenticationService.getAuthFromRequest(req).login;
-    const user = await userService.getUserByLogin(login);
-    await authValidator.validate(new UserTargetsItself(db, user.id, userId));
-
-    await await carService.createCarForUser(carName, userId);
-    res.send();
-  } catch (err) {
-    if (err instanceof IllegalArgumentError) {
-      next(createHttpError(400, err.message));
-      return;
-    } else if (err instanceof NotFoundError) {
-      next(createHttpError(404, err.message));
-      return;
-    } else if (err instanceof UnauthorizedError) {
-      next(createHttpError(403, err.message));
+    if (!reqValidation.isEmpty()) {
+      const error = reqValidation.errors[0];
+      next(createHttpError(400, `Bad request : ${error.path} is invalid`));
       return;
     }
 
-    next(createHttpError(500, "Internal error while creating car"));
+    try {
+      const login = authenticationService.getAuthFromRequest(req).login;
+      const user = await userService.getUserByLogin(login);
+      await authValidator.validate(new UserTargetsItself(db, user.id, userId));
+
+      await await carService.createCarForUser(carName, userId);
+      res.send();
+    } catch (err) {
+      if (err instanceof IllegalArgumentError) {
+        next(createHttpError(400, err.message));
+        return;
+      } else if (err instanceof NotFoundError) {
+        next(createHttpError(404, err.message));
+        return;
+      } else if (err instanceof UnauthorizedError) {
+        next(createHttpError(403, err.message));
+        return;
+      }
+
+      next(createHttpError(500, "Internal error while creating car"));
+    }
   }
-});
+);
 
 module.exports = router;
